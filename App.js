41be420fe3a110254acb4e0f943331d50aef9037
Ruchir Gupta <ruchir98@gmail.com>
2023-01-15 20:13:26 +0530

    added parcel

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading1 = React.createElement('h1', {id: 'heading1'}, 'Helllllio react world from app.js');
 const heading2 = React.createElement('h2', {id: 'heading2'}, 'Ruchir Gupta');
 
