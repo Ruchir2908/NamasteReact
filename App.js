a3bfe7bb91408f9e36ce63d1f8f855d05b88b6b8
Ruchir Gupta <ruchir98@gmail.com>
2023-01-15 20:25:50 +0530

    used JSX

--- App.js
+++ App.js
@@ -1,10 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading1 = React.createElement('h1', {id: 'heading1'}, 'Helllllio react world from app.js');
-const heading2 = React.createElement('h2', {id: 'heading2'}, 'Ruchir Gupta');
-
-const container = React.createElement('div', {id: 'container'}, [heading1, heading2]);
+const JSXUse = () => {
+    return <h1>JSX used here in React</h1>
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(container);
+root.render(<JSXUse/>);
